2e988040d911191beb36b8babfa1f1dd84c3e82d
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2022-06-15 22:27:56 +0200

    Initial commit - copy pasting flavienbwk boilerplate.

---
+++ api/prod.entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# These commands are here because they must be run at runtime
+
+set -x
+python -m my_app db init
+python -m my_app db migrate --message 'Initial database migration'
+python -m my_app db upgrade
+python -m my_app run
